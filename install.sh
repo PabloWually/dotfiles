1e98684120a7b4a13a45140b96b36f6805128082
Pablo Guallichico <pablo.guallichico@outlook.com>
2025-12-24 13:35:05 -0500

    feat(config): add install script and rename tmux config
    Adds a bash script to create symbolic links for tmux, opencode, and starship
    configurations. Renames tmux config file from .tmux.config to .tmux.conf for
    standard naming convention.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Adding links to configuration files
+ln -sf ~/.config/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/.config/dotfiles/opencode/ ~/.config
+ln -sf ~/.config/dotfiles/starship/starship.toml ~/.config/starship.toml
